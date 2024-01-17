0911672bd7f3727446dbb5c25f91991a6b9ba391
Python <svetloewill@mail.ru>
2024-01-17 19:52:45 +0300

    2

--- main.R
+++ main.R
@@ -1,1 +1,17 @@
-a=5
+# Определение переменных
+num1 <- 5
+num2 <- 10
+
+# Вычисление суммы
+sum <- num1 + num2
+
+# Вывод результата на экран
+# print(sum)
+# print('Hello, World!')
+# print(5+5)
+# a <- 5
+# b <- 44
+# n <- a + b
+# print(n)
+# print(mean(c(1,2,3)))
+iris
